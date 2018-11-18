aae9e57cf9c60f670402b5cef26a7e3d9afcb7f9
Neil Clavin <neil@neils-macbook-pro.local>
2018-11-18 17:27:19 +0100

    change 2 UIMages to blue and red (tint not working)

--- DoppelAutoOverlap/ViewController.swift
+++ DoppelAutoOverlap/ViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+extension UIImageView {
+    func setImageColor(color: UIColor) {
+        let templateImage = self.image?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+        self.image = templateImage
+        self.tintColor = color
+    }
+}
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var image2: UIImageView!
+    
+    
+    
+    func TintRed(chooseImage: UIImageView) {
+    
+    
+    let templateImage = chooseImage.image!.withRenderingMode(.alwaysTemplate)
+    chooseImage.image = templateImage
+       
+    chooseImage.tintColor = UIColor.red
+        
+        
+    }
+    
+    func TintBlue(chooseImage: UIImageView) {
+        
+        
+        let templateImage = chooseImage.image!.withRenderingMode(.alwaysTemplate)
+        chooseImage.image = templateImage
+        
+        chooseImage.tintColor = UIColor.blue
+        
+        
+    }
+
+    func TintPurple() {
+        let imageView = UIImageView(image: UIImage(named: "image1"))
+        imageView.setImageColor(color: UIColor.purple)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        TintRed(chooseImage: image1)
+        //TintBlue(chooseImage: image2)
+        TintPurple()
+        
+        
+        
+        
     }
 
-
+    
+    
 }
 
