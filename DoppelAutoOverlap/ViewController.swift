6cb95cd6d9d27ca4e2a0bde640984bbace631b75
Neil Clavin <neil@neils-mbp.local>
2018-11-18 16:12:45 +0100

    Initial Commit

---
+++ DoppelAutoOverlap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoppelAutoOverlap
+//
+//  Created by Neil Clavin on 18/11/2018.
+//  Copyright © 2018 Neil Clavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
